dbf87072dcead0c0e961a140d4efecd8b8935adf
Shablinskii Stanislav Ivanovith <lxlllokillxl@gmail.com>
2025-04-08 15:56:33 +0300

    Codo.py

--- Codo.py
+++ Codo.py
@@ -1,25 +1,46 @@
 import streamlit as st
 import firebase_admin
 from firebase_admin import credentials, db
+import json
 import io
 from PIL import Image
+import matplotlib.pyplot as plt
+import numpy as np
+import sympy as sp
+import threading
+import time
 
-# Ініціалізація Firebase через Streamlit Secrets
+# Функція для завантаження перекладів
+def load_translations(lang):
+    try:
+        with open(f"translations/{lang}.json", "r", encoding="utf-8") as file:
+            return json.load(file)
+    except FileNotFoundError:
+        st.error(f"Помилка завантаження: файл '{lang}.json' не знайдено.")
+        return {}
+    except Exception as e:
+        st.error(f"Сталася помилка: {e}")
+        return {}
+
+# Ініціалізація Firebase через Streamlit Secrets (перевіряємо, чи вже ініціалізовано)
 if not firebase_admin._apps:
     cred = credentials.Certificate({
-        "type": st.secrets["firebase_key"]["type"],
-        "project_id": st.secrets["firebase_key"]["project_id"],
-        "private_key_id": st.secrets["firebase_key"]["private_key_id"],
-        "private_key": st.secrets["firebase_key"]["private_key"],
-        "client_email": st.secrets["firebase_key"]["client_email"],
-        "client_id": st.secrets["firebase_key"]["client_id"],
-        "databaseURL": st.secrets["firebase_key"]["databaseURL"]
+        "type": st.secrets["firebase"]["type"],
+        "project_id": st.secrets["firebase"]["project_id"],
+        "private_key_id": st.secrets["firebase"]["private_key_id"],
+        "private_key": st.secrets["firebase"]["private_key"].replace("\\n", "\n"),
+        "client_email": st.secrets["firebase"]["client_email"],
+        "client_id": st.secrets["firebase"]["client_id"],
+        "auth_uri": st.secrets["firebase"]["auth_uri"],
+        "token_uri": st.secrets["firebase"]["token_uri"],
+        "auth_provider_x509_cert_url": st.secrets["firebase"]["auth_provider_x509_cert_url"],
+        "client_x509_cert_url": st.secrets["firebase"]["client_x509_cert_url"]
     })
     firebase_admin.initialize_app(cred, {
-        "databaseURL": st.secrets["firebase_key"]["databaseURL"]
+        'databaseURL': st.secrets["firebase"]["databaseURL"]
     })
 
-# Функція для збереження фото в Firebase
+# Функція для завантаження фото у Firebase
 def upload_photo(image_file):
     image_data = image_file.read()
     image_base64 = image_data.hex()  # Конвертація у HEX
@@ -29,7 +50,6 @@ st.title("Sm: Галерея фото на Firebase")
 
 # Завантаження фото
 uploaded_file = st.file_uploader("Завантажте фото", type=["jpg", "png", "jpeg"])
-
 if uploaded_file:
     image_data = upload_photo(uploaded_file)
     description = st.text_area("Опишіть фото тут")
